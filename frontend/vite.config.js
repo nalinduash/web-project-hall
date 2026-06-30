942367507bc404806c44e14b6ce12f52194d1f32
kavidu717 <dushmanthakavidu143@gmail.com>
2026-06-30 20:23:38 +0530

    build(frontend): initialize react project packages and vite runner config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+       "@": path.resolve(__dirname, "./src"),
     }
   }
 })
